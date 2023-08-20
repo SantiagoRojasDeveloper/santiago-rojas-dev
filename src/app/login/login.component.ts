72befbe499c5ff3328a8d9fc324b4dc339643e21
Manuel Chaparro <manuel_chaparro@gtccorporation.com>
2023-08-20 00:09:33 -0600

    Vistas y Pipe en titulos

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public name: string;
+  public splitedName: string;
 
-  ngOnInit(): void {
+  constructor() { 
+    this.name = "Manuel Santiago Chaparro Rojas";
+    this.splitedName = "";
   }
 
+  ngOnInit(): void {
+    const dividedName: Array<string> = this.name.split("");
+    let i = 0;
+    let showPipe: boolean = true;
+  
+    const intervalId = setInterval(() => {
+      if (i < dividedName.length) {
+        if(showPipe){
+          this.splitedName += dividedName[i] + "|";
+          showPipe = false;
+        }else{
+          this.splitedName += dividedName[i];
+          this.splitedName = this.splitedName.replace(/\|/g , "");
+          showPipe = true;
+        }
+        
+        i++;
+      } else {
+        clearInterval(intervalId);
+        this.showAndHidePipe(true);
+        this.showTitle();
+      }
+    }, 150); // Intervalo de 5 segundos
+  }
+
+  private showAndHidePipe(show: boolean){ 
+    
+    const intervalId = setInterval(() => {
+      this.splitedName = this.splitedName.replace(/\|/g , "");
+      if(show){
+        this.splitedName += " |";
+        show = false;
+      }else{
+        show = true;
+      }
+    }, 1000);
+
+    
+  }
+
+  private showTitle(){
+    
+  }
 }
